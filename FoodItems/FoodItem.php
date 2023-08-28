2690abb235532eb31d98e0318939fc29d99b4572
yuki-gakiya <yuki-gakiya@web-server.test-server.com>
2023-08-28 17:30:19 +0900

    親クラスと子クラスのconstを作成

---
+++ FoodItems/FoodItem.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace FoodItems;
+
+abstract class FoodItem{
+    private string $name;
+    private string $description;
+    private float $price;
+
+    public function __construct(string $name, string $description, float $price) {
+        $this->name = $name;
+        $this->description = $description;
+        $this->price = $price;
+    }
+}
