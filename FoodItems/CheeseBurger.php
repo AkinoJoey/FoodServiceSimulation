dfa4714857aac23b9c35647efc5591beea53dfb2
AkinoJoey <gakiya213@gmail.com>
2023-09-01 17:45:09 +0900

    定数を削除してgetCategory()を修正

--- FoodItems/CheeseBurger.php
+++ FoodItems/CheeseBurger.php
@@ -3,10 +3,8 @@
 namespace FoodItems;
 
 class CheeseBurger extends FoodItem{
-    public const CATEGORY = "Burger";
-
     public function __construct() {
-        parent::__construct("Cheese Burger","Classic burger with delicious cheese.",800,10);
+        parent::__construct("Cheese Burger","Classic burger with delicious cheese.",5,10);
     }
 
     public static function getCategory(): string
